0667ca69010224c86362a0de70cacf3c96250976
Renzo Sy <renzo@Renzos-MacBook-Air.local>
2023-04-19 12:06:46 +0800

    Added form layout for data (task) input

--- src/AddTask.tsx
+++ src/AddTask.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import { Button } from '@chakra-ui/react'
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Textarea,
+  Input,
+  Flex,
+  Divider,
+  Spacer,
+} from "@chakra-ui/react";
 
-type Props = {}
+type Props = {};
 
 export default function AddTask() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Button  margin={4} colorScheme='teal'> Add New Task </Button>
-  )
+    <>
+      <Button margin={4} colorScheme="teal" onClick={onOpen}>
+        {" "}
+        Add New Task{" "}
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent fontFamily="Roboto Mono">
+          <ModalHeader>Add Task</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex marginTop={5} marginBottom={5} justify="space-between" direction="row" height="5em">
+              <FormControl>
+                <FormLabel>Date Added</FormLabel>
+                <Input placeholder="Date Added" size="s" type="date" />
+                <FormHelperText fontSize="2xs">Input the task's date added.</FormHelperText>
+              </FormControl>
+              <Divider orientation="vertical" marginLeft={4} marginRight={4}/>
+              <FormControl>
+                <FormLabel>Date Due</FormLabel>
+                <Input placeholder="Date Due" size="s" type="date" />
+                <FormHelperText fontSize="2xs">Input the task's date due.</FormHelperText>
+              </FormControl>
+            </Flex>
+
+            <Divider marginBottom={4} />
+            <FormLabel>Task</FormLabel>
+            <Input placeholder="Task" />
+
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <Textarea placeholder="Input type description here..." />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="teal" mr={3} onClick={onClose}>
+              Add Task
+            </Button>
+            <Button variant="ghost">Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
